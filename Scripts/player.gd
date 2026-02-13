b8516b72090254c4b51423533a80c46ab9b52d6d
Caden <cadendouglas05@gmail.com>
2026-02-13 10:39:57 -0600

    did some work, added some code and worked on the title screen

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,8 +7,13 @@ var coyote_time: float = 0.15
 var jump_buffer_time: float = 0.15 
 var coyote_timer: float = 0.0
 var jump_buffer_timer: float = 0.0
-
-
+var weapons_library = {
+	"choc_steak" : preload("res://Scenes/Weapons/dark_choc_steak.tscn")
+}
+@onready var weapon_slot = $weapon_slot
+@onready var ui = $main_ui
+func _ready() -> void:
+	ui.weapon_selected.connect(equip_weapon)
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		coyote_timer = coyote_time 
@@ -33,3 +38,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+func equip_weapon(weapon_name: String):
+	if not weapons_library.has(weapon_name):
+		print("weapon could not be found")
+		return
+	for child in weapon_slot.get_children():
+		child.queue_free()
+	var weapon_scene = weapons_library[weapon_name]
+	var new_weapon = weapon_scene.instantiate()
+	weapon_slot.add_child(new_weapon)
+	
