af33990259f34b65080e755f0648eeefbd374807
Caden <cadendouglas05@gmail.com>
2026-03-02 00:37:15 -0600

    various export changes

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -12,7 +12,7 @@ func _process(_delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
 
 
 func _on_start_button_mouse_entered() -> void:
